fe358f48067ce1a107a3e4ff663ec9332a50ef2a
Alia Taha <aliataha5538@gmail.com>
2022-07-18 00:50:51 -0500

    eventListener for cellElements

--- Main.js
+++ Main.js
@@ -1,23 +1,16 @@
-// storing board information in an array
-let boardData = [
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-];
-
-// return all cell elements from DOM
+// declare and pull CSS selectors from DOM
 const cellElements = document.querySelectorAll(".cell");
 console.log(cellElements);
 
-// add eventListener awaiting user action
-// able to detect which cell is clicked
+// add eventListener to await user action
+// index is able to detect which cell is clicked
 cellElements.forEach((cell, index) => {
   cell.addEventListener("click", () => {
-    placeMarker(index);
+    savedData(index);
   });
 });
 
-// placing markers in each cell clicked
-function placeMarker(index) {
+// saving data in each cell
+function savedData(index) {
   console.log(index);
 }
